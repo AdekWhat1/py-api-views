d9deab2aaf12a884ea9d5681c67e65baa543d073
Den <mr.denisyakubenko@gmail.com>
2026-07-09 20:42:22 +0200

    feat: created different views(generics/viewset/APIView) and configured routes

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    MovieViewSet,
+    CinemaHallViewSet,
+    GenreList, GenreDetail, ActorList, ActorDetail,
+
+)
+
+router = routers.DefaultRouter()
+
+router.register("movies", MovieViewSet, basename="movie")
+router.register("cinema_halls", CinemaHallViewSet, basename="cinema_hall")
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre_detail"),
+
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor_detail"),
+
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
